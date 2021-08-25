f82e4a18805f14e87025896696cb240c6530c354
evmurphy <evmurphy@ncsu.edu>
2021-08-25 10:55:26 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Ellie Murphy and is
+        <a
+          href="https://github.com/ellievmurphy/react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        by GitHub
+      </footer>
+    </div>
+  );
 }
