24f718bcfec9a43bb5c18e440d6bc49773c52c4b
evmurphy <evmurphy@ncsu.edu>
2021-08-25 10:57:13 -0400

    Updated link settings

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
         <a
           href="https://github.com/ellievmurphy/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced
         </a>{" "}
