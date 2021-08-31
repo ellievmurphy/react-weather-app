6d321f682a0e703f125bca3805295908f149015b
evmurphy <evmurphy@ncsu.edu>
2021-08-30 22:25:22 -0400

    Added API integration

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather app</h1>
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Ellie Murphy and is{" "}
           <a
