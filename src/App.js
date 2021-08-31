3aae94c5fb3567186751570f075d6722be0dc5c0
evmurphy <evmurphy@ncsu.edu>
2021-08-30 21:43:43 -0400

    Updated css for Weather

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
+import Weather from "./Weather";
 
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App container">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Ellie Murphy and is{" "}
-        <a
-          href="https://github.com/ellievmurphy/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        by GitHub
-      </footer>
+    <div className="App">
+      <div className="container">
+        <h1>Weather app</h1>
+        <Weather />
+        <footer>
+          This project was coded by Ellie Murphy and is{" "}
+          <a
+            href="https://github.com/ellievmurphy/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          by GitHub
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from "axios";
+
+import "./Weather.css";
+
+export default function Weather() {
+  const apiKey = "58998f2f1d96bf70dbdd7f7a20868eb4";
+  let city = "New York";
+  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search for a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="Weather icon"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
